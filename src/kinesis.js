c3fedafb8f413a37366558f99c8e6773bb8cd1d9
litsynp <nocte55is@gmail.com>
2022-10-24 20:23:44 +0900

    chore: remove unused console.logs

--- src/kinesis.js
+++ src/kinesis.js
@@ -9,52 +9,39 @@ import {
 
 import { AWS_REGION, KINESIS_STREAM_NAME } from './config.js'
 
-export async function publishEvent({ data }) {
-  const id = uuid()
-
+export async function publishEvent({ id = uuid(), data }) {
   const client = new KinesisClient({
     region: AWS_REGION,
   })
-  const command = new PutRecordCommand({
-    Data: new TextEncoder().encode(
-      JSON.stringify({
-        data,
-      }),
-    ),
-    PartitionKey: id,
-    StreamName: KINESIS_STREAM_NAME,
-  })
-  console.log(command)
-  const result = await client.send(command)
-  console.log(result)
-  return result
+  return client.send(
+    new PutRecordCommand({
+      Data: new TextEncoder().encode(
+        JSON.stringify({
+          data,
+        })
+      ),
+      PartitionKey: id,
+      StreamName: KINESIS_STREAM_NAME,
+    })
+  )
 }
 
 export async function getShardIterator({ shardId: ShardId }) {
   const client = new KinesisClient({
     region: AWS_REGION,
   })
-  const command = new GetShardIteratorCommand({
-    ShardId,
-    StreamName: KINESIS_STREAM_NAME,
-    ShardIteratorType: ShardIteratorType.TRIM_HORIZON,
-  })
-  console.log(command)
-  const result = await client.send(command)
-  console.log(result)
-  return result
+  return client.send(
+    new GetShardIteratorCommand({
+      ShardId,
+      StreamName: KINESIS_STREAM_NAME,
+      ShardIteratorType: ShardIteratorType.TRIM_HORIZON,
+    })
+  )
 }
 
-export async function getRecords({
-  shardIterator: ShardIterator,
-  limit: Limit,
-}) {
+export async function getRecords({ shardIterator: ShardIterator, limit: Limit = 10 }) {
   const client = new KinesisClient({
     region: AWS_REGION,
   })
-  const command = new GetRecordsCommand({ ShardIterator, Limit })
-  console.log(command)
-  const result = await client.send(command)
-  console.log(result)
-  return result
+  return client.send(new GetRecordsCommand({ ShardIterator, Limit }))
 }
